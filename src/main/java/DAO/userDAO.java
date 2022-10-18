311873513f17c946e6dc9216b0479c09c1fc6354
StewieD <doanhaidan345@gmail.com>
2022-10-18 11:44:21 +0700

    StewieD updated userDAO&DAOinterface 18/10/2022

--- src/main/java/DAO/userDAO.java
+++ src/main/java/DAO/userDAO.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import com.mysql.cj.x.protobuf.MysqlxConnection.Close;
+
 import Model.User;
 import database.JDBCUtil;
 
@@ -58,5 +60,104 @@ public class userDAO implements DAOinterface<User> {
 		} catch (Exception e) {
 		}
 			return ketQua;	
+	}
+	@Override
+	public void update(User t) {
+		Connection conn = JDBCUtil.getConnection();
+		int check = 0;
+		try {
+			String sql = " UPDATE user SET firstname= ?, lastname= ?, password= ? WHERE username= ?;";
+			
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, t.getFirstName());
+			st.setString(2, t.getLastName());
+			st.setString(3, t.getPassWord());
+			st.setString(4, t.getUserName());
+			System.out.println(sql);
+			check = st.executeUpdate();
+			System.out.println("Changed " +check+ " line. ");
+		JDBCUtil.closeConnection(conn);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	@Override
+	public void delete(User t) {
+		Connection conn = JDBCUtil.getConnection();
+		int check =0;
+		try {
+			String sql = "DELETE FROM user\r\n"
+					+ "WHERE username= ?; ";
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, t.getUserName());
+			
+			check = st.executeUpdate();
+			System.out.println("Changed "+check+" line.");
+			JDBCUtil.closeConnection(conn);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	@SuppressWarnings("null")
+	@Override
+	public ArrayList<User> selectAll() {
+		
+		ArrayList<User> List = new ArrayList<User>();
+		try {
+			Connection conn = JDBCUtil.getConnection();
+			String sql="select * from user ; ";
+			
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet rs = st.executeQuery();
+			
+			while(rs.next()) {
+				String fname = rs.getString("firstname");
+				String lname = rs.getString("lastname");
+				String username = rs.getString("username");
+				String password = rs.getString("password");
+				User user = new User(fname, lname, username, password);
+				List.add(user);
+			}
+			System.out.println(List);
+			JDBCUtil.closeConnection(conn);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return List;
+	}
+	@Override
+	public User selectByPrimaryKey(User t) {
+		User kq = null;
+		try {
+			Connection conn = JDBCUtil.getConnection();
+			String sql = "select * from user where username= ?;";
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, t.getUserName());
+			ResultSet rs = st.executeQuery();
+			while(rs.next()) {
+				kq = new User(rs.getString("firstname"), rs.getString("lastname"), rs.getString("username"), rs.getString("password"));
+			}
+			JDBCUtil.closeConnection(conn);
+ 		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return kq;
+	}
+	@Override
+	public ArrayList<User> selectByCondition(String Condition) {
+		ArrayList<User> List  = new ArrayList<User>();
+		try {
+			Connection conn = JDBCUtil.getConnection();
+			String sql = "select * from user where "+Condition+" ;" ;
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet rs = st.executeQuery();
+			while(rs.next()) {
+				User u = new User(rs.getString("firstname"), rs.getString("lastname"), rs.getString("username"), rs.getString("password"));
+				List.add(u);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return List;
 	}
 }
--- src/main/java/test/testLogin.java
+++ src/main/java/test/testLogin.java
@@ -7,7 +7,14 @@ import Model.User;
 
 public class testLogin {
 	public static void main(String[] args) {
-	User user = new User("Dan", "Doan Hai", "haidan0", "zxc");
-	userDAO.getInstance().checkLogin(user);
+	User user = new User("Dang", "Hoang", "xinhxan", "zxc123");
+//	userDAO.getInstance().checkLogin(user);
+//	userDAO.getInstance().update(user);
+//	userDAO.getInstance().delete(user);
+//	ArrayList<User> ListUser = userDAO.getInstance().selectAll();
+//	for (User user : ListUser) {
+//		System.out.println(user.toString());
+//	}
+	System.out.println(userDAO.getInstance().selectByCondition("firstname='Teo'"));	
 	}
 }
